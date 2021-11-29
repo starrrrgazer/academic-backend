0843222299c242da859febb366d33e64b32f4dc3
starrrrgazer <70183278+starrrrgazer@users.noreply.github.com>
2021-11-29 17:26:29 +0800

    创建项目

---
+++ Community/src/main/java/com/example/community/dao/DemoRepository.java
@@ -0,0 +1,12 @@
+package com.example.community.dao;
+//数据层,通过xml对数据库进行一个具体的操作
+
+
+import com.example.community.entity.demo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DemoRepository extends JpaRepository<demo, Long> {
+    demo findByNumber(int num);
+}
