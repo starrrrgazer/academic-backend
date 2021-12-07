68c7de66ba642aeedcad70c957afdc4db25fa43d
starrrrgazer <70183278+starrrrgazer@users.noreply.github.com>
2021-12-07 22:59:21 +0800

    跨域连接

--- Community/src/main/java/com/example/community/CommunityApplication.java
+++ Community/src/main/java/com/example/community/CommunityApplication.java
@@ -3,8 +3,10 @@ package com.example.community;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableRedisHttpSession
 public class CommunityApplication {
 
     public static void main(String[] args) {
