cce6c32e4523c287a9ccfae6564602c9b2042413
lowestbuaaer <70185479+lowestbuaaer@users.noreply.github.com>
2021-12-08 16:20:00 +0800

    未完成版搜索接口

---
+++ Literature/src/main/java/com/example/literature/service/DemoService.java
@@ -0,0 +1,9 @@
+package com.example.literature.service;
+//服务层，用于实现后端的逻辑（写调用的方法），并且这里要调用数据层dao
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoService {
+    //这里定义接口函数
+}
