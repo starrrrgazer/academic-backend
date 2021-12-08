8b413d2bcf940d5072a48942308b3e3a6f53d88b
starrrrgazer <70183278+starrrrgazer@users.noreply.github.com>
2021-12-08 10:33:36 +0800

    report

--- Community/src/main/java/com/example/community/entity/QuestionFollow.java
+++ Community/src/main/java/com/example/community/entity/QuestionFollow.java
@@ -1,15 +1,13 @@
 package com.example.community.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "QuestionFollow")
 public class QuestionFollow {
     @Id
     @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(name = "questionID")
