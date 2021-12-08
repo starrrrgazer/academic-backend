d72de1dcd44b55e458eba909ed906885719bb4e1
熊奕翔 <1984936632@qq.com>
2021-12-08 15:18:43 +0800

    j

--- Management/src/main/java/com/example/management/Controller/PaperController.java
+++ Management/src/main/java/com/example/management/Controller/PaperController.java
@@ -5,6 +5,7 @@ import com.example.management.Entity.Venue;
 import com.example.management.Service.PaperService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -21,15 +22,15 @@ public class PaperController {
 
 
     @PostMapping("/addPaper")
-    public Map<String,Object> addPaper(Map<String,Object> map){
+    public Map<String,Object> addPaper(@RequestBody Map<String,Object> map){
         return paperService.addPaper(map);
     }
 
     @PostMapping("/deletePaper")
-    public Map<String,Object> deletePaper(Map<String,Object> map){
+    public Map<String,Object> deletePaper(@RequestBody Map<String,Object> map){
         return paperService.deletePaper(map);
     }
 
     @PostMapping("/getArticleList")
-    public Map<String,Object> getArticleList(Map<String,Object> map){ return paperService.getArticleList(map);}
+    public Map<String,Object> getArticleList(@RequestBody Map<String,Object> map){ return paperService.getArticleList(map);}
 }
