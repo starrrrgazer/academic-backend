e58d8f0f3a8e8f8f3d457e5e39f297aed09b975f
starrrrgazer <70183278+starrrrgazer@users.noreply.github.com>
2021-12-18 23:52:09 +0800

    都添加了redis的配置

--- Literature/src/main/java/com/example/literature/LiteratureApplication.java
+++ Literature/src/main/java/com/example/literature/LiteratureApplication.java
@@ -2,7 +2,10 @@ package com.example.literature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
 @SpringBootApplication
+@EnableRedisHttpSession
 public class LiteratureApplication {
 
     public static void main(String[] args) {
