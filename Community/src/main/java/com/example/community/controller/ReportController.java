0d4a6aac6994003ac7521413834d01288cac67aa
starrrrgazer <70183278+starrrrgazer@users.noreply.github.com>
2021-12-21 23:05:32 +0800

    修改了部署所需要的跨域配置

--- Community/src/main/java/com/example/community/controller/ReportController.java
+++ Community/src/main/java/com/example/community/controller/ReportController.java
@@ -17,7 +17,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = {"http://localhost:8000","http://localhost:80","http://localhost:443"},allowCredentials = "true",maxAge = 3600)
+@CrossOrigin(origins = {"http://localhost:8000","http://localhost:80","http://localhost:443",
+        "http://121.36.60.6:8000","http://121.36.60.6:80","http://121.36.60.6:443",
+        "https://121.36.60.6:8000","https://121.36.60.6:80","https://121.36.60.6:443"},allowCredentials = "true",maxAge = 3600)
 @RestController
 public class ReportController {
     @Autowired
